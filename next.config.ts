4df5611eda56c25c7c07f796b1acab20f52a0808
nadeemjn <partonline34@gmail.com>
2024-11-14 17:29:35 +0500

    Updated next.config.js for Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  // optional: for static file handling
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
